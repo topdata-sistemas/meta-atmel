42143c40c5b2848c5d2d4bf023e4c0421498270e
Balakrishnan Sambath <balakrishnan.s@microchip.com>
2024-11-13 14:00:00 +0530

    recipes-connectivity: wilc-demo-fs-overlay: enhance STA mode wilc demo scripts
    Update Start_STA.sh with kea-dhcp compatibility, fix command sequence,
    indentations and shellcheck warnings.
    
    Signed-off-by: Balakrishnan Sambath <balakrishnan.s@microchip.com>
    Signed-off-by: Hari Prasath Gujulan Elango <hari.prasathge@microchip.com>

--- recipes-connectivity/wilc-ble-apps/files/Start_STA.sh
+++ recipes-connectivity/wilc-ble-apps/files/Start_STA.sh
@@ -19,19 +19,18 @@ echo "---------------------------------------------------"
 echo "---------------------------------------------------"
 
 if lsmod | grep -q "wilc_sdio" ; then
-        echo "1.############## WILC-SDIO module is available ##############"
+	echo "1.############## WILC-SDIO module is available ##############"
 else
-        echo "1.############## Inserting the wilc-sdio module ##############"
-        modprobe wilc-sdio
-        if lsmod | grep -q "wilc_sdio";  then
-                echo "WILC-SDIO module insterted successfully"
-        else
-                echo "WILC-SDIO module insert failed"
-		exit 0
-        fi
+	echo "1.############## Inserting the wilc-sdio module ##############"
+	modprobe wilc-sdio
+	if lsmod | grep -q "wilc_sdio";  then
+		echo "WILC-SDIO module insterted successfully"
+	else
+		echo "WILC-SDIO module insert failed"
+		exit 1
+	fi
 fi
 
-if ! test -f /etc/wpa_supplicant.conf; then
 cat << 'EOT' > /etc/wpa_supplicant.conf
 ctrl_interface=/var/run/wpa_supplicant
 ctrl_interface_group=0
@@ -41,7 +40,6 @@ network={
 	key_mgmt=NONE
 }
 EOT
-fi
 
 if grep -q "ssid" /etc/wpa_supplicant.conf; then
 	echo "Connecting to router:-"
@@ -49,30 +47,41 @@ if grep -q "ssid" /etc/wpa_supplicant.conf; then
 	sleep 2
 else
 	echo "Input the SSID of the router/AP"
-	read newSsid
-	echo "New SSID " $newSsid
+	read -r newSsid
+	echo "New SSID ""$newSsid"
 	sed -i "s/{.*/& \n\tssid=\"$newSsid\"/gI" /etc/wpa_supplicant.conf
 	echo "Input the passphrase(if non-secured, press Carriage Return(Enter)"
-	read passPhrase
-	if [ "$passPhrase" = "" ];then
+	read -r passPhrase
+	if [ "$passPhrase" = "" ]; then
 		echo "Connecting to Non-Secured AP"
-		sed -i "s/\bkey_mgmt\b.*/\tkey_mgmt=\"NONE\"/gI" /etc/wpa_supplicant.conf
+		sed -i "s/\bkey_mgmt\b.*/key_mgmt=NONE/gI" /etc/wpa_supplicant.conf
 	else
-		echo "New Passphrase " $passPhrase
+		echo "New Passphrase ""$passPhrase"
 		sed -i "s/ssid.*/& \n\tpsk=\"$passPhrase\"/gI" /etc/wpa_supplicant.conf
 		sed -i "/key_mgmt/d" /etc/wpa_supplicant.conf
 	fi
-
 fi
-echo "2.########### Stopping hostapd if running any ###########"
 
-systemctl is-active hostapd@open --quiet
-[  $? -eq 0 ] && systemctl stop hostapd@open
+echo "2.########### Stopping kea DHCP service if running ###########"
+if pgrep -f "kea-dhcp" > /dev/null; then
+	keactrl stop
+fi
 
-systemctl is-active hostapd@wpa --quiet
-[  $? -eq 0 ] && systemctl stop hostapd@wpa
+echo "3.########### Stopping hostapd if running any ###########"
+if systemctl is-active hostapd@open --quiet; then
+	systemctl stop hostapd@open
+fi
 
-echo "3.############## Bringing up wlan0 interface ##############"
+if systemctl is-active hostapd@wpa --quiet; then
+	systemctl stop hostapd@wpa
+fi
+
+sleep 2
+echo "4.############# Loading network configuration  #######"
+cp /lib/systemd/network/80-wifi-station.network.example /etc/systemd/network/wlan0.network
+networkctl reload
+
+echo "5.############## Bringing up wlan0 interface ##############"
 if ifconfig | grep -q "wlan0" ; then
 	echo "Wireless LAN interface is UP!"
 else
@@ -84,15 +93,13 @@ else
 		echo "WILC-SDIO module insterted successfully"
 	else
 		echo "WILC-SDIO module insert failed"
-		exit 0
+		exit 2
 	fi
 fi
 
-cp /lib/systemd/network/80-wifi-station.network.example /etc/systemd/network/wlan0.network
-networkctl reload
-
-echo "4.############## Starting WPA Supplicant  ##################"
+sleep 4
+echo "6.############## Starting WPA Supplicant  ##################"
 systemctl restart wpa_supplicant.service
 
-echo "5.############## Restart systemd-networkd service ##########"
+echo "7.############## Restart systemd-networkd service ##########"
 systemctl restart systemd-networkd.service
