topdata-sistemas/meta-atmel

commit 81e17715fa8c40de65026699be359476b0a2a850

kea: install custom kea dhcp4 conf
Install the custome kea dhcp4 configuration file that let's the server
provide IP addressed on interface wlan-0.

Signed-off-by: Hari Prasath Gujulan Elango <hari.prasathge@microchip.com>

recipes-connectivity/kea/kea_%.bbappend
@@ -1,3 +1,8 @@
 FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
 
 SRC_URI:append = " file://0001-keactrl.in-create-var-lib-kea-and-var-run-kea-folder.patch"
+
+do_install:append () {
+    install -d ${D}${sysconfdir}/kea
+    install -m 0644 ${WORKDIR}/kea-dhcp4.conf ${D}${sysconfdir}/kea/kea-dhcp4.conf
+}

recipes-connectivity/kea/kea/kea-dhcp4.conf
@@ -1,0 +1,25 @@
+/*
+ *This configuration file sets up Kea DHCPv4 to provide IP addresses on interface
+ *wlan0.It assigns IPs in the range 192.168.1.100 to 192.168.1.200 within the
+ *subnet 192.168.1.0/24.
+ */
+
+{
+  "Dhcp4": {
+    "interfaces-config": {
+      "interfaces": ["wlan0"]
+    },
+    "subnet4": [
+      {
+        "subnet": "192.168.1.0/24",
+        "pools": [
+          { "pool": "192.168.1.100 - 192.168.1.200" }
+        ],
+        "option-data": [
+          { "code": 3, "data": "192.168.1.1" },
+          { "code": 6, "data": "192.168.1.1, 8.8.8.8" }
+        ]
+      }
+    ]
+  }
+}

commit eccb21162c2b618daaf40acc289fbfd14abec068

kea: add conf file to SRC_URI to fix compilation issue
Add the kea-dhcp4.conf file to SRC_URI to fix compilation issue.

Fixes: 81e17715fa8c ("kea: install custom kea dhcp4 conf”)
Signed-off-by: Hari Prasath Gujulan Elango <hari.prasathge@microchip.com>

recipes-connectivity/kea/kea_%.bbappend
@@ -1,6 +1,8 @@
 FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
 
-SRC_URI:append = " file://0001-keactrl.in-create-var-lib-kea-and-var-run-kea-folder.patch"
+SRC_URI:append = " file://0001-keactrl.in-create-var-lib-kea-and-var-run-kea-folder.patch \
+		   file://kea-dhcp4.conf \
+"
 
 do_install:append () {
     install -d ${D}${sysconfdir}/kea